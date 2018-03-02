8a8b18d48ce5ff4bc2e02322a23216fbc9669ed8
Anthony Sottile <asottile@umich.edu>
2018-03-01 21:13:56 -0800

    WIP

--- tests/pyupgrade_test.py
+++ tests/pyupgrade_test.py
@@ -10,11 +10,14 @@ from pyupgrade import _fix_dictcomps
 from pyupgrade import _fix_format_literals
 from pyupgrade import _fix_long_literals
 from pyupgrade import _fix_octal_literals
+from pyupgrade import _fix_percent_format
 from pyupgrade import _fix_sets
 from pyupgrade import _fix_unicode_literals
 from pyupgrade import _imports_unicode_literals
+from pyupgrade import _simplify_conversion_flag
 from pyupgrade import main
 from pyupgrade import parse_format
+from pyupgrade import parse_percent_format
 from pyupgrade import unparse_parsed_string
 
 
@@ -329,6 +332,189 @@ def test_noop_octal_literals(s, expected):
 @pytest.mark.xfail(sys.version_info >= (3,), reason='python2 "feature"')
 def test_fix_octal_literal():
     assert _fix_octal_literals('0755') == '0o755'
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        (
+            '""', (
+                ('""', None),
+            ),
+        ),
+        (
+            '"%%"', (
+                ('"', (None, None, None, None, '%')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"%s"', (
+                ('"', (None, None, None, None, 's')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"%s two! %s"', (
+                ('"', (None, None, None, None, 's')),
+                (' two! ', (None, None, None, None, 's')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"%(hi)s"', (
+                ('"', ('hi', None, None, None, 's')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"%()s"', (
+                ('"', ('', None, None, None, 's')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"%#o"', (
+                ('"', (None, '#', None, None, 'o')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"% #0-+d"', (
+                ('"', (None, ' #0-+', None, None, 'd')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"%5d"', (
+                ('"', (None, None, '5', None, 'd')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"%*d"', (
+                ('"', (None, None, '*', None, 'd')),
+                ('"', None),
+            )
+        ),
+        (
+            '"%.f"', (
+                ('"', (None, None, None, '.', 'f')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"%.5f"', (
+                ('"', (None, None, None, '.5', 'f')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"%.*f"', (
+                ('"', (None, None, None, '.*', 'f')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"%ld"', (
+                ('"', (None, None, None, None, 'd')),
+                ('"', None),
+            ),
+        ),
+        (
+            '"%(complete)#4.4f"', (
+                ('"', ('complete', '#', '4', '.4', 'f')),
+                ('"', None),
+            ),
+        ),
+    ),
+)
+def test_parse_percent_format(s, expected):
+    assert parse_percent_format(s) == expected
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        ('', ''),
+        (' ', ' '),
+        ('   ', ' '),
+        ('#0- +', '#<+'),
+        ('-', '<'),
+    ),
+)
+def test_simplify_conversion_flag(s, expected):
+    assert _simplify_conversion_flag(s) == expected
+
+
+@pytest.mark.parametrize(
+    's',
+    (
+        # cannot determine whether `unknown_type` is tuple or not
+        '"%s" % unknown_type',
+        # format of bytestring cannot be changed to `.format(...)`
+        'b"%s" % (b"bytestring",)',
+        # See https://bugs.python.org/issue16806, we have incorrect offsets
+        '"""%s\n""" % ("issue16806",)',
+        # out-of-order parameter consumption
+        '"%*s" % (5, "hi")', '"%.*s" % (5, "hi")',
+        # potential conversion to int required
+        '"%d" % (flt,)', '"%i" % (flt,)', '"%u" % (flt,)',
+        # potential conversion to character required
+        '"%c" % (some_string,)',
+        # different output vs .format() in python 2
+        '"%#o" % (123,)',
+        # no format equivalent
+        '"%()s" % {"": "empty"}',
+        # different output in python2 / python 3
+        '"%4%" % ()',
+    ),
+)
+def test_percent_format_noop(s):
+    assert _fix_percent_format(s) == s
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        ('"trivial" % ()', '"trivial".format()'),
+        ('"%s" % ("simple",)', '"{}".format("simple")'),
+        ('"%s" % ("%s" % ("nested",),)', '"{}".format("{}".format("nested"))'),
+        ('"%s%% percent" % (15,)', '"{}% percent".format(15)'),
+        ('"%3f" % (15,)', '"{:3f}".format(15)'),
+        ('"%-5s" % ("hi",)', '"{:<5}".format("hi")'),
+        (
+            'paren_continue = (\n'
+            '    "foo %s "\n'
+            '    "bar %s" % (x, y)\n'
+            ')\n',
+            'paren_continue = (\n'
+            '    "foo {} "\n'
+            '    "bar {}".format(x, y)\n'
+            ')\n',
+        ),
+        (
+            'paren_string = (\n'
+            '    "foo %s "\n'
+            '    "bar %s"\n'
+            ') % (x, y)\n',
+            'paren_continue = (\n'
+            '    "foo {} "\n'
+            '    "bar {}"\n'
+            ').format(x, y)\n',
+        ),
+        (
+            '"%s" % (\n'
+            '    "keep trailing comma",\n'
+            ')',
+            '"{}".format(\n'
+            '    "keep trailing comma",\n'
+            ')',
+        ),
+    ),
+)
+def test_percent_format(s, expected):
+    assert _fix_percent_format(s) == expected
 
 
 def test_main_trivial():
